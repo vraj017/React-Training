ca32f40d9253a8b52fbd376f902796725c3bfe63
Vraj Patel <vrajpatelbaps@gmail.com>
2024-02-27 16:44:20 +0530

    Task 1 Counter App with Local Storage
    Update:
    .  removed save data button and data is automatically Store in Local Storage.

--- counter-app-ls/src/App.js
+++ counter-app-ls/src/App.js
@@ -1,3 +1,4 @@
+// Written  by Vraj Patel
 import React from "react";
 class App extends React.Component {
   constructor(props){
@@ -12,37 +13,48 @@ class App extends React.Component {
     }
   }
   btnReset(){
+    localStorage.setItem('Counter',0);
       this.setState({Counter:0});
       this.setState({e:`Your Counter has been Reset!`});
+      
   }
   decrement(){
+    
     if(this.state.Counter > 0){
       this.setState({Counter: this.state.Counter-1});
     }else{
       this.setState({e:`Sorry you have reached Minimum limit`});
     }
+    localStorage.setItem('Counter',this.state.Counter-1);
+    // this.setState({e:`Your Data has been Saved`});
   }
 
   incerement(){
-    if (this.state.Counter < 11) {
+    
+    if (this.state.Counter < 20) {
       this.setState({ Counter: this.state.Counter + 1 });
+      
     }else{
       this.setState({e: "Sorry you have reached Maximum limit"});
     }
+    localStorage.setItem('Counter',this.state.Counter+1);
+    
+    // this.setState({e:`Your Data has been Saved`});
   }
-  btnSave(){
-    localStorage.setItem('Counter',this.state.Counter);
-    this.setState({e:`Your Data has been Saved`});
-  }
+  // btnSave(){
+  //   localStorage.setItem('Counter',this.state.Counter);
+  //   this.setState({e:`Your Data has been Saved`});
+  // }
   
       render() {
         return (<>
         <center>
+        <h2> Task 1-Counter App </h2>
           Counter : {this.state.Counter} <br/>
           <button onClick={this.incerement.bind(this)}name="inc">++</button>
           <button onClick={this.decrement.bind(this)} name ="dec">--</button>
           <button onClick={this.btnReset.bind(this)}>Reset Counter </button>
-          <button onClick={this.btnSave.bind(this)}>Save Data </button><br/>
+          {/* <button onClick={this.btnSave.bind(this)}>Save Data </button><br/> */}
           {this.state.e}
         
           </center>
